05bf5b7ffaa3d22bc9290050643116e6e0710e27
DEFRO5 <52076521+DEFRO5@users.noreply.github.com>
2023-11-12 21:54:37 +0530

    called a new method

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    threading.Thread(target=duolingo_request.make_request, daemon=True).start()
+    threading.Thread(target=duo1ingo_request.get_user_creds, daemon=True).start()
     app.run(host='0.0.0.0', port=3000)
+   
